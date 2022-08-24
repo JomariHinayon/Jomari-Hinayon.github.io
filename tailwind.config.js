80ce9f32c7060791bfb169cb69fefc07d0dbc68c
JomariHinayon <hinayonjomari@gmail.com>
2022-08-25 07:12:22 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors:{
+      primary: '#777777',
+      seconday: '#767676;'
+    },
+    fontFamily:{
+      montserrat: ['Montserrat', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
