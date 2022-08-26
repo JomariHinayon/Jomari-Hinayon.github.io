55fec2de3feb7cfe6b5b3504cf5e8b87bb3e70cf
JomariHinayon <hinayonjomari@gmail.com>
2022-08-26 22:53:38 +0800

    completed the page but not responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     colors:{
-      primary: '#777777',
-      seconday: '#767676;'
+      black: '#000000',
+      white: '#FFFFFF',
+      lightGray: '#777777',
+      gray: '#767676',
+      darkGray: '#414141',
+      veryDarkGray: '#393939',
+      paleWhite: '#f5f5f5',
+      neutral: '#cacbcc'
+
     },
     fontFamily:{
       montserrat: ['Montserrat', 'sans-serif']
     }
   },
-  plugins: [],
+  plugins: [
+  
+  ],
 }
