a38565cb3307a855b77d4cb0bccdcd0ecc422b2d
JomariHinayon <hinayonjomari@gmail.com>
2022-08-29 01:48:10 +0800

    add scoll animations and make navbar fixed

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('hidden');
 });
 
+
+
+
+
 // Dark Mode-------------->
 
 // Icons
@@ -54,3 +58,110 @@ moonIcon.addEventListener('click', () => {
 themeCheck();
 
 // <---------------------------------------------
+
+
+
+
+// Scroll Reveal ---------------------------------->
+var slideDown = {
+    distance: '50%',
+    origin: 'top',
+    duration: 1500,
+    opacity: 0,
+    interval: 500
+};
+
+var slideRight = {
+    distance: '50%',
+    origin: 'left',
+    duration: 1500,
+    opacity: 0
+};
+
+var slideLeft = {
+    distance: '50%',
+    origin: 'right',
+    duration: 1500,
+    opacity: 0
+};
+
+var slideUp = {
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1500,
+    opacity: 0         
+};
+
+var slideUp2 = {
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1200,
+    opacity: 0,
+    interval: 200    
+};
+
+var interval = {
+    interval: 200,
+    origin: 'top',
+    distance: '50%',
+    duration: 1000
+};
+
+var fadeIn = {
+   opacity: 0,
+   duration: 2500 
+}
+
+// Animate 
+ScrollReveal().reveal('.slide-right', slideRight);
+ScrollReveal().reveal('.slide-left', slideLeft);
+ScrollReveal().reveal('.slide-up', slideUp);
+ScrollReveal().reveal('.slide-down', slideDown);
+ScrollReveal().reveal('.interval', interval);
+ScrollReveal().reveal('.slide-up2', slideUp2);
+ScrollReveal().reveal('.fade-in', fadeIn);
+
+//My Picture opacity
+ScrollReveal().reveal('.my-pic', {distance: 0, opacity: 0,duration: 2500, delay: 500});
+
+// My Projects Preset Reveal Animation
+ScrollReveal().reveal('.project1', slideRight);
+ScrollReveal().reveal('.project2', {delay: 500, distance: '50%', origin: 'top', duration: 1500, opacity: 0});
+ScrollReveal().reveal('.project3', {delay: 800, distance: '50%', origin: 'bottom', duration: 1500, opacity: 0});
+ScrollReveal().reveal('.project4', {delay: 1200, distance: '50%', origin: 'right', duration: 1500, opacity: 0});
+// <-------------------------------------------------------------
+
+
+// ScrollSpy ===============================>
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav a');
+let menuItems = document.getElementById('menu-items');
+
+window.onscroll = () => {
+    
+    section.forEach(sec => {
+
+        let screenWidth = screen.width;
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 200; 
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector(' a[href*='+ id +']').classList.add('active');
+            })
+        }
+        if(top > 100 && screenWidth >= 1120){
+            menuItems.classList.remove('lg:container');
+            menuItems.classList.add('fixed');
+        }else{
+             menuItems.classList.remove('fixed');
+        }
+    });
+}
+
+// <========================================================
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,44 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    colors:{
-      black: '#000000',
-      white: '#FFFFFF',
-      lightGray: '#777777',
-      gray: '#767676',
-      darkGray: '#414141',
-      veryDarkGray: '#393939',
-      paleWhite: '#f5f5f5',
-      neutral: '#cacbcc',
-      white2: '#F7F7F7',
-      white3: '#E7E6E2',
-      white4: '#DADADA',
-      white5: '#CCCCCC'
+    extend:{
+      colors:{
+        black: '#000000',
+        white: '#FFFFFF',
+        lightGray: '#777777',
+        gray: '#767676',
+        darkGray: '#414141',
+        veryDarkGray: '#393939',
+        paleWhite: '#f5f5f5',
+        neutral: '#cacbcc',
+        white2: '#F7F7F7',
+        white3: '#E7E6E2',
+        white4: '#DADADA',
+        white5: '#CCCCCC',
+        blue: '#0000FF',
+        green: '#00FF00',
+        yellow: '#FFFF00',
+        orange: '#FFA500'
 
-    },
-    fontFamily:{
-      montserrat: ['Montserrat', 'sans-serif']
-    },
-    screens:{
-      'sm': '64px',
-      'md': '768px',
-      'lg': '1100px',
-      'contact': '819px',
-      'navbar' : '1122px',
-      'nav': '1250px'
+      },
+      fontFamily:{
+        montserrat: ['Montserrat', 'sans-serif']
+      },
+      screens:{
+        'sm': '64px',
+        'md': '768px',
+        'lg': '1100px',
+        'contact': '819px',
+        'navbar' : '1122px',
+        'nav': '1250px',
+        'smLogo': '500px'
+      },
+      animation:{
+        'bounce1': 'bounce 1s infinite',
+        'bounce2': 'bounce 1.1s infinite',
+        'bounce3': 'bounce 1.2s infinite',
+        'bounce4': 'bounce 1.3s infinite'
+      }
     }
   },
   darkMode: 'class',
